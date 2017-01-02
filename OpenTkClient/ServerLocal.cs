4c7d049aa0821e36983a0dfab932c4e28caac177
Sean <onewheel@gmail.com>
2017-01-02 23:00:35 +1300

    local server

--- OpenTkClient/ServerLocal.cs
+++ OpenTkClient/ServerLocal.cs
@@ -1,13 +1,14 @@
-﻿using Sean.Shared.Comms;
-using System;
-using System.Threading;
+﻿using System;
 using Sean.Shared;
 using System.Collections.Generic;
+using Sean.WorldGenerator;
 
 namespace OpenTkClient
 {
     public class ServerLocal : IServer
     {
+        private World WorldInstance = new World();
+
         public void Start()
         {
             Console.WriteLine ("Local Server starting");
@@ -15,11 +16,8 @@ namespace OpenTkClient
 
         public void GetWorldMap()
         {
-            ClientConnection.BroadcastMessage(new Message()
-            {
-                WorldMapRequest = new WorldMapRequestMessage()
-            }
-            );
+            MapManager.SetWorldMapHeight(WorldInstance.GlobalMap);
+            MapManager.SetWorldMapTerrain(WorldInstance.GlobalMapTerrain);
         }
 
         private static List<string> sent = new List<string>(); // TODO - do better
@@ -29,63 +27,10 @@ namespace OpenTkClient
 			if (!sent.Contains(hash))
 			{
 				sent.Add(hash);
-				ClientConnection.BroadcastMessage(new Message()
-					{
-						MapRequest = new MapRequestMessage()
-						{
-							Coords = new ChunkCoords(x,z)
-						}
-					}
-				);
+                var coords = new ChunkCoords (x,z);
+                var chunk = WorldInstance.GetChunk(coords);
+                MapManager.AddChunk (coords, chunk);
 			}
 		}
-
-        private void ProcessMessage(Guid clientId, Message msg)
-        {
-            try
-            {
-                //Console.WriteLine ($"Processing response... {msg.ToString ()}");
-                if (msg.Map != null)
-                {
-                    var position = msg.Map.MinPosition;
-                    var coords = new ChunkCoords (position);
-                    var chunk = Sean.Shared.Chunk.Deserialize (coords, msg.Data);
-                    MapManager.AddChunk (coords, chunk);
-                }
-                if (msg.WorldMapResponse != null)
-                {
-                    var size = new ArraySize()
-                    {
-                        scale = msg.WorldMapResponse.Scale,
-                        minX = msg.WorldMapResponse.MinPosition.X,
-                        minY = msg.WorldMapResponse.MinPosition.Y,
-                        minZ = msg.WorldMapResponse.MinPosition.Z,
-                        maxX = msg.WorldMapResponse.MaxPosition.X,
-                        maxY = msg.WorldMapResponse.MaxPosition.Y,
-                        maxZ = msg.WorldMapResponse.MaxPosition.Z,
-                    };
-                    var map = new Array<byte>(size);
-                    map.DeSerialize(msg.Data);
-                    if (msg.WorldMapResponse.MapRequestType == Sean.Shared.Comms.MapRequestType.HeightMap)
-                        MapManager.SetWorldMapHeight(map);
-                    else if (msg.WorldMapResponse.MapRequestType == Sean.Shared.Comms.MapRequestType.Terrain)
-                        MapManager.SetWorldMapTerrain(map);
-                }
-                if (msg.MapCharacterUpdate != null)
-                {
-                    CharacterManager.UpdateLocation (msg.MapCharacterUpdate.CharacterId, msg.MapCharacterUpdate.Position);
-                }
-                if (msg.MapUpdate != null)
-                {
-                    MapManager.SetBlock (msg.MapUpdate.Position, msg.MapUpdate.NewBlock);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception caught in ProcessMessage - {0}", e.ToString());
-            }
-        }
-
     }
-
 }
